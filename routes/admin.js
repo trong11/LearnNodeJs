bc0af2e9b042b8afb6ee13b00c8591e83313b30a
TrongNp <trongnp.nqd@gmail.com>
2022-09-28 19:04:58 +0700

    MVC

---
+++ routes/admin.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const rootDir = require('../util/path');
+const router = express.Router();
+const products = [];
+const productsController = require('../controllers/products');
+
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct);
+
+module.exports = router;
---
+++ routes/shop.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const express = require('express');
+const productController = require('../controllers/products');
+const router = express.Router();
+
+router.get('/', productController.getProducts );
+
+module.exports = router;
